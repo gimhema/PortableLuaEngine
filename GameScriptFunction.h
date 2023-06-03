6791e6c9e8df2aed7aca50823f851fd17f257c7a
gimhema <49871739+gimhema@users.noreply.github.com>
2023-06-04 02:01:53 +0900

    add GameScriptFunction.h

---
+++ GameScriptFunction.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "stdafx.h"
+
+namespace GameScriptFunctionForLua
+{
+	int HelloLua(lua_State* LuaObj);
+
+	int SomeCPPFunction(lua_State* LuaObj);
+
+	int SomeCPPFunction2(lua_State* LuaObj);
+
+	int SomeCPPFunction3(lua_State* LuaObj);
+
+	int SomeCPPFunction4(lua_State* LuaObj);
+
+}
