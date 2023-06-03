ebacd5f6906a558169bbe3332f8705b6157bcdbd
gimhema <49871739+gimhema@users.noreply.github.com>
2023-06-04 02:00:41 +0900

    Update LuaEngineCore.h

--- LuaEngineCore.h
+++ LuaEngineCore.h
@@ -1,22 +1,57 @@
 #pragma once
-#include <string>
+#include "stdafx.h"
 
 
-class LuaEngineCore 
+class LuaScriptManager 
 {
 public:
-	// Constructor & Deconstructor
-	LuaEngineCore();
-	~LuaEngineCore();
+	LuaScriptManager();
+	~LuaScriptManager();
 
 public:
-	// Property
-
-public:
-	// Method
-	void Initailize();
-	void LoadLuaScript(std::string filePath);
-	void LuaExecution();
+	std::vector<std::string> LuaActionNameVec; 
+	std::unordered_map<std::string, lua_State*> LuaActionMap;
 	
 
+public:
+	virtual lua_State* GetLuaActionByName(std::string name);
+	virtual void Initialize();
+	virtual void InitializeLuaActioName();
+	virtual void AddFunctionRequirements(); // 스크립트 실행에 필요한 함수들을 Add해줍니다.
+	// Non Programmer들은 AddFunctionRequirements에서 Add된 CPP함수들만 사용할 수 있습니다.
+	// Requirements를 공유하는 루아 액션(파일)들을 로드합니다.
+	// Non Programmer들이 작업한 내용이 갱신됩니다.
+	virtual void AddAllLuaAction();
+	virtual void CallLuaActionByName(std::string name);
 };
+
+class EventLuaScriptManager : public LuaScriptManager
+{
+public:
+	EventLuaScriptManager();
+	~EventLuaScriptManager();
+
+public:
+	void AddFunctionRequirements();
+};
+
+class MonsterAIScriptManager : public LuaScriptManager
+{
+public:
+	MonsterAIScriptManager();
+	~MonsterAIScriptManager();
+
+public:
+	void AddFunctionRequirements();
+};
+
+class RaidScriptManager : public LuaScriptManager
+{
+public:
+	RaidScriptManager();
+	~RaidScriptManager();
+
+public:
+	void AddFunctionRequirements();
+};
+
